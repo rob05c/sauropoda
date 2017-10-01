2f302c3d517dfb0f2479075f1adc4f5eaa9c593d
Robert Butts <robert.o.butts@gmail.com>
2017-10-01 15:24:00 +0000

    Add create user UI, on login page

--- static/login.js
+++ static/login.js
@@ -19,3 +19,25 @@ function login() {
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhttp.send(JSON.stringify({user:name, pass:pass}));
 }
+
+function create() {
+  console.log('create calling');
+  var name = document.getElementById("name").value;
+  var pass = document.getElementById("pass").value;
+  console.log('create calling ' + name + ":" + pass);
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        window.location.href = '/';
+        document.getElementById("loginFail").textContent = "Success! Redirecting...";
+      } else {
+        document.getElementById("loginFail").textContent = "Someone already has that name.";
+      }
+    }
+  };
+  xhttp.open("POST", '/api/createuser', true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(JSON.stringify({user:name, pass:pass}));
+}
